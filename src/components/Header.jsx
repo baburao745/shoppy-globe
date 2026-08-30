2aa8a3fd26aab738fe083d715e4406af81f4abb9
baburao745 <aby5399@gmail.com>
2026-08-30 18:29:48 +0530

    Style cart item count badge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const items = useSelector((state) => state.cart.items);
+
+  const cartCount = items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
   return (
     <header className="header">
       <Link to="/" className="logo">
@@ -10,7 +18,15 @@ function Header() {
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
-        <Link to="/cart">🛒 Cart</Link>
+
+        <Link to="/cart">
+          🛒 Cart
+          {cartCount > 0 && (
+            <span className="cart-count">
+              {cartCount}
+            </span>
+          )}
+        </Link>
       </nav>
     </header>
   );
